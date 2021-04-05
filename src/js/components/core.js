694a0942a9cbbc665b0a5ee34711149f3df72a29
Tiah Campbell <cuevastl@gmail.com>
2021-04-04 22:23:16 -0400

    Added css styling

--- src/js/components/core.js
+++ src/js/components/core.js
@@ -6,7 +6,6 @@ export default function core(coreExercises){
             return `
             <li class="Content">
                 <h4 class="exerciseName" id="${coreExercise.id}">${coreExercise.name}</h4>
-                <button class="addExercise" id="${coreExercise.id}">Add Exercise To My Workout</button>
             </li>
             `
         }).join("")}
--- src/js/components/exerciseDetails.js
+++ src/js/components/exerciseDetails.js
@@ -2,12 +2,12 @@ export default function exerciseDetails(exercise){
     console.log(exercise);
     return `
         <h1>${exercise.name}</h1>
-        <ul class="Content">
-            <li>Exercise Description: ${exercise.description}</li>
-            <ol class="Index">
+        <ul class="DetailsContent">
+            Exercise Description: ${exercise.description}
+            <ol class="DetailsIndex">
             ${exercise.muscles.map(muscle =>{
             return `
-                <li class="Content">
+                <li class="Details">
                     <h4 class="muscleName" id="${muscle.id}">${muscle.name}</h4>
                     <img class="muscleImage" src="https://wger.de${muscle.image_url_main}"/>
                 </li>
